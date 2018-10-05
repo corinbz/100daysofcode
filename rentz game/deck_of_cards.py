8bae8ed534a64494665c5066bb8e8556d48ee0a5
corinbz <craciuncorin5@gmail.com>
2018-10-05 19:57:44 +0200

    Done the deck and draw card function

---
+++ rentz game/deck_of_cards.py
@@ -0,0 +1,43 @@
+import random
+
+
+# Create the deck of cards with suits and values (for loop?)
+def make_deck():
+
+	suits = ['Hearts' , 'Diamonds', 'Spades', 'Clubs']
+	values = range(2, 15)
+	deck = []
+
+	for s in suits:
+		for v in values:
+			deck.append('{} of {}'.format(v,s))
+
+
+	return deck
+
+deck_used = make_deck()
+drawed_card = []
+
+# draw a random card (pop it)
+def draw_card(cards_to_draw):
+
+	temp = cards_to_draw
+	
+	while temp > 0:
+		rand_card = random.randint(0,len(deck_used))
+		print(deck_used.pop(rand_card))
+		temp -= 1
+
+
+
+
+def test():
+	print(len(deck_used))
+	# pass
+
+
+
+
+make_deck()
+draw_card(2)
+test()
